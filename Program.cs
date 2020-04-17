16b06af02f56844ac49329c5fda190d9d54c1005
jandresleal <62621149+jandresleal@users.noreply.github.com>
2020-04-17 18:37:45 -0300

    los 3 nombres

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace ejercicio2
         static void Main(string[] args)
         {
             Console.WriteLine("Melisa Benítez");
+            Console.WriteLine("Mariana Roso");
+            Console.WriteLine("Juan Andrés Leal");
         }
     }
 }
